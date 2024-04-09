96400408342a592863e1526556b46a356409225c
Kate Koltsova <kate.petronyuk@gmail.com>
2024-04-09 22:10:40 +0300

    Added positions list func

---
+++ app/Http/Controllers/API/V1/PositionController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace app\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PositionResource;
+use App\Models\Position;
+use Illuminate\Http\Request;
+
+class PositionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $positions = Position::orderBy('id', 'asc')->get();
+
+        if (empty($positions)) {
+            return response()->json(['success' => false, 'message' => 'Positions not found'], 404);
+        }
+
+        $response = [
+            'success' => true,
+            'positions' => PositionResource::collection($positions)
+        ];
+
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api/api_v1.php
+++ routes/api/api_v1.php
@@ -2,3 +2,6 @@
 
 Route::resource('users', \App\Http\Controllers\Api\V1\UserController::class)
     ->only('index');
+
+Route::resource('positions', \App\Http\Controllers\Api\V1\PositionController::class)
+    ->only('index');
